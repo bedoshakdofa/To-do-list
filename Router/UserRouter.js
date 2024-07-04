04845c08505fe690f20e0c9736a6836da5719ec8
bedoshakdofa <abdullahshakdofa@gmail.com>
2024-07-04 12:03:57 +0300

    modified

--- Router/UserRouter.js
+++ Router/UserRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post("/sginup", usercontrol.signup);
 router.post("/login", usercontrol.login);
 router.post("/logout", usercontrol.LogOut);
-router.patch("/confirmEmail/:token", usercontrol.confirmEmail);
+router.get("/confirmEmail/:token", usercontrol.confirmEmail);
 router.post("/forgetpassword", usercontrol.forgetpassword);
 router.patch("/restpassword/:token", usercontrol.restpassword);
 router.use(usercontrol.protect);
